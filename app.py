3a9a976610d8c2188a930358def29ab8f39a1adb
Bertrand TORNIL <bertrand.tornil@cafe.com>
2014-07-07 18:18:44 +0200

    Github mode

--- app.py
+++ app.py
@@ -1,19 +1,57 @@
 import subprocess
-from flask import Flask
+from flask import (
+    Flask,
+    request,
+    g,
+    session, redirect, url_for,
+    render_template_string,
+    jsonify
+)
+from flask.ext.github import (
+    GitHub,
+    GitHubError
+)
+
+SECRET_KEY = 'development key'
+DEBUG = True
+
+GITHUB_CLIENT_ID = '9b818f868e6191cf674e'
+GITHUB_CLIENT_SECRET = '0423ed3a012ac8e50e9bd5d54c78b70ab9dc4000'
+GITHUB_CALLBACK_URL = 'http://localhost:5000/github-callback'
+
 
 app = Flask(__name__, static_url_path='', static_folder='')
-
+app.config.from_object(__name__)
+github = GitHub(app)
 
 bashCommand = "git --git-dir=/{repository_path}/.git --work-tree=/{repository_path} log --format='%ai|%an'"
 
 
+@app.before_request
+def before_request():
+    g.user = None
+    if 'user_id' in session:
+        g.user = session['user_id']
+
+
 @app.route('/')
-def root():
+def index():
+    if g.user:
+        t = 'Hello! <a href="{{ url_for("user") }}">Get user</a> ' \
+            '<a href="{{ url_for("logout") }}">Logout</a>'
+    else:
+        t = 'Hello! <a href="{{ url_for("login") }}">Login</a>'
+
+    return render_template_string(t)
+
+
+@app.route('/explore')
+def explore():
     return app.send_static_file('index.html')
 
 
 @app.route("/data/<path:repository_path>")
-def main(repository_path):
+def data(repository_path):
     process = subprocess.Popen(bashCommand.format(repository_path=repository_path).split(), stdout=subprocess.PIPE)
     (output, error) = process.communicate()
 
@@ -23,6 +61,130 @@ def main(repository_path):
         return '\n'.join([x[1:-1] for x in output.split('\n')])
     else:
         return (error, 500, [])
+
+
+@app.route('/login')
+def login():
+    return github.authorize()
+
+
+@app.route('/logout')
+def logout():
+    session.pop('user_id', None)
+    return redirect(url_for('index'))
+
+
+@app.route('/github-callback')
+@github.authorized_handler
+def authorized(oauth_token):
+    next_url = request.args.get('next') or url_for('index')
+    if oauth_token is None:
+        return redirect(next_url)
+
+    user = oauth_token
+    session['user_id'] = user
+    return redirect(url_for('index'))
+
+
+@github.access_token_getter
+def token_getter():
+    user = g.user
+    if user is not None:
+        return user
+
+
+@app.route('/user')
+def user():
+    return str(github.get('user'))
+
+
+@app.route('/repo/<owner>/<repo>')
+def repo(owner, repo):
+    # GET /repos/:owner/:repo
+    repo_infos = github.get('repos/%s/%s' % (owner, repo))
+
+    # GET /repos/:owner/:repo/stats/contributors
+    repo_stats_contributors = github.get('repos/%s/%s/stats/contributors' % (owner, repo))
+
+    # GET /repos/:owner/:repo/stats/commit_activity
+    repo_stats_commit_activity = github.get('repos/%s/%s/stats/commit_activity' % (owner, repo))
+
+    # GET /repos/:owner/:repo/stats/code_frequency
+    repo_stats_code_frequency = github.get('repos/%s/%s/stats/code_frequency' % (owner, repo))
+
+    # GET /repos/:owner/:repo/stats/participation
+    repo_stats_participation = github.get('repos/%s/%s/stats/participation' % (owner, repo))
+
+    # GET /repos/:owner/:repo/stats/punch_card
+    repo_stats_punch_card = github.get('repos/%s/%s/stats/punch_card' % (owner, repo))
+
+    return jsonify({
+        'infos': repo_infos,
+        'stats_contributors': repo_stats_contributors,
+        'stats_commit_activity': repo_stats_commit_activity,
+        'stats_code_frequency': repo_stats_code_frequency,
+        'stats_participation': repo_stats_participation,
+        'stats_punch_card': repo_stats_punch_card
+    })
+
+
+@app.route('/commits/<owner>/<repo>')
+def commits(owner, repo):
+    """
+    Must returns commits formated as :
+    2013-11-22 22:04:05 +0100|Bertrand TORNIL
+    2013-10-21 22:59:38 +0200|Bertrand TORNIL
+    2013-10-21 12:05:41 +0200|Bertrand TORNIL
+
+    ... etc.
+    """
+    # re-implement the github call, to provide pagination
+    # links are in the header
+    result = []
+
+    response = raw_request('GET', '%srepos/%s/%s/commits' % (github.BASE_URL, owner, repo))
+    status_code = str(response.status_code)
+
+    if status_code.startswith('4'):
+        raise GitHubError(response)
+
+    assert status_code.startswith('2')
+
+    for commit in response.json():
+        result.append(commit['commit']['committer'])
+        #{
+        #   "name": "Monalisa Octocat",
+        #   "email": "support@github.com",
+        #   "date": "2011-04-14T16:00:49Z"
+        # },
+
+    while hasattr(response, 'links') and 'next' in response.links:
+        response = raw_request('GET', response.links['next']['url'])
+        status_code = str(response.status_code)
+        if status_code.startswith('4'):
+            raise GitHubError(response)
+
+        assert status_code.startswith('2')
+        for commit in response.json():
+            result.append(commit['commit']['committer'])
+
+    return '\n'.join(['%s|%s' % (row['date'], row['name']) for row in result])
+
+
+def raw_request(method, url, params=None, **kwargs):
+    """
+    Makes a HTTP request and returns the raw
+    :class:`~requests.Response` object.
+
+    """
+    if params is None:
+        params = {}
+
+    if 'access_token' not in params:
+        params['access_token'] = github.get_access_token()
+
+    return github.session.request(
+        method, url, params=params, allow_redirects=True, **kwargs)
 
 
 if __name__ == "__main__":
--- js/explository.js
+++ js/explository.js
@@ -1,106 +1,142 @@
-(function(window) {
 
-  window.onload = function() {
+window.onload = function() {
 
-    var form = document.getElementById("form");
-    form.addEventListener('submit',function(e) {
-      e.preventDefault();
+  var form = document.getElementById("form");
+  form.addEventListener('submit',function(e) {
+    e.preventDefault();
 
-      var inputPath = document.getElementById("input-path").value;
+    var inputPath = document.getElementById("input-path").value;
 
-      dsv("/data"+inputPath, render).response(function(request) {
-
-        return dsv.parseRows(request.responseText, function(d) {
-          // format style :
-          // git log --format='%ai|%an'
-          // datetime|author
-          // "2013-10-17 18:14:56 +0200|Bertrand TORNIL"
-          var datetime = datetimeFormat.parse(d[0]);
-          return {
-            datetime: datetime,
-            date : new Date(datetime.getFullYear(),
-                            datetime.getMonth(),
-                            datetime.getDate()),
-            author: d[1].toLowerCase()
-          };
-        });
-
-      });
+    dsv("/commits/"+inputPath, render).response(function(request) {
 
       document.getElementById("fun-stats").className = "";
 
-      return false;
+      return dsv.parseRows(request.responseText, function(d) {
+        // datetime|author
+        // "2013-10-17T18:14:56Z|Bertrand TORNIL"
+        var datetime = datetimeFormat.parse(d[0]);
+        return {
+          datetime: datetime,
+          date : new Date(datetime.getFullYear(),
+                          datetime.getMonth(),
+                          datetime.getDate()),
+          author: d[1].toLowerCase()
+        };
+      });
 
-    },false);
-  };
+    });
 
+    return false;
+
+  }, false);
+};
+
+
+//
+// GIT statistics
+//
+
+var authorsChart = dc.rowChart(".authors-chart");
+var hourOfDayChart = dc.barChart(".hour-of-day-chart");
+var dayOfWeekChart = dc.rowChart(".day-of-week-chart");
+var volumeChart = dc.barChart(".daily-volume-chart");
+
+var dateFormat = d3.time.format("%Y-%m-%d");
+// var datetimeFormat = d3.time.format("%Y-%m-%d %H:%M:%S %Z");
+// "2014-04-01T01:33:28Z"
+var datetimeFormat = d3.time.format("%Y-%m-%dT%H:%M:%SZ");
+
+var dsv = d3.dsv("|", "text/plain");
+
+var render = function(data) {
 
   //
-  // GIT statistics
+  // I. Data part
+  //
+  var ndx = crossfilter(data);
+
+  var all = ndx.groupAll();
+
+  // authors
+  var author = ndx.dimension(function(d) {
+    return d.author;
+  })
+  var authorGroup = author.group();
+
+  // hours of the day
+  var hourOfDay = ndx.dimension(function (d) {
+    var hour = d.datetime.getHours();
+    return hour;
+  });
+  var hourOfDayGroup = hourOfDay.group();
+
+  // days of the week
+  var dayOfWeek = ndx.dimension(function (d) {
+    var day = d.datetime.getDay();
+    var name=["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+    return day + "." + name[day];
+  });
+  var dayOfWeekGroup = dayOfWeek.group();
+
+  // commits by day
+  var commitByDays = ndx.dimension(function (d) {
+    return d.date;
+  });
+  var commitByDaysGroup = commitByDays.group();
+
+  //
+  // II. Chart parts
   //
 
-  var authorsChart = dc.rowChart(".authors-chart");
-  var hourOfDayChart = dc.barChart(".hour-of-day-chart");
-  var dayOfWeekChart = dc.rowChart(".day-of-week-chart");
-  var volumeChart = dc.barChart(".daily-volume-chart");
 
-  var dateFormat = d3.time.format("%Y-%m-%d");
-  var datetimeFormat = d3.time.format("%Y-%m-%d %H:%M:%S %Z");
+  // authors
+  authorsChart.width(990)
+              .height(30 * authorGroup.size())
+              .margins({top: 10, left: 10, right: 10, bottom: 30})
+              .dimension(author)
+              .group(authorGroup)
+              // .ordering(function(d) {
+              //    return d.value;
+              // })
+              .colors(d3.scale.category20())
+              .label(function (d) {
+                  return d.key;
+              })
+              .elasticX(true)
+              .xAxis().ticks(4);
 
-  var dsv = d3.dsv("|", "text/plain");
-
-  var render = function(data) {
-
-    //
-    // I. Data part
-    //
-    var ndx = crossfilter(data);
-
-    var all = ndx.groupAll();
-
-    // authors
-    var author = ndx.dimension(function(d) {
-      return d.author;
-    })
-    var authorGroup = author.group();
-
-    // hours of the day
-    var hourOfDay = ndx.dimension(function (d) {
-      var hour = d.datetime.getHours();
-      return hour;
-    });
-    var hourOfDayGroup = hourOfDay.group();
-
-    // days of the week
-    var dayOfWeek = ndx.dimension(function (d) {
-      var day = d.datetime.getDay();
-      var name=["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
-      return day + "." + name[day];
-    });
-    var dayOfWeekGroup = dayOfWeek.group();
-
-    // commits by day
-    var commitByDays = ndx.dimension(function (d) {
-      return d.date;
-    });
-    var commitByDaysGroup = commitByDays.group();
-
-    //
-    // II. Chart parts
-    //
-
-    // authors
-    authorsChart.width(990)
-                .height(30 * authorGroup.size())
-                .margins({top: 10, left: 10, right: 10, bottom: 30})
-                .group(authorGroup)
-                .dimension(author)
-                .ordering(function(d, i) {
-                   return -d.value;
+  // hours of the day
+  hourOfDayChart.width(620)
+                .height(180)
+                .margins({top: 10, right: 10, bottom: 30, left: 40})
+                .dimension(hourOfDay)
+                .group(hourOfDayGroup)
+                .elasticY(true)
+                .gap(1)
+                .round(dc.round.floor)
+                .x(d3.scale.linear().domain([0, 24]))
+                .renderHorizontalGridLines(true)
+                .filterPrinter(function (filters) {
+                    var filter = filters[0], s = "";
+                    s += parseInt(filter[0], 10) + "h -> " + parseInt(filter[1], 10) + "h";
+                    return s;
                 })
-                .colors(d3.scale.category20())
+                .xAxis()
+                .tickFormat(function (v) {
+                    return v + "h";
+                });
+
+  hourOfDayChart.yAxis().ticks(5);
+
+  // day of the week
+  dayOfWeekChart.width(360)
+                .height(30 * dayOfWeekGroup.size())
+                .margins({top: 10, left: 10, right: 10, bottom: 30})
+                .group(dayOfWeekGroup)
+                .dimension(dayOfWeek)
+                .colors(d3.scale.category10())
                 .label(function (d) {
-                    return d.key;
+                    return d.key.split(".")[1];
                 })
                 .title(function (d) {
                     return d.value;
@@ -108,64 +144,23 @@
                 .elasticX(true)
                 .xAxis().ticks(4);
 
-    // hours of the day
-    hourOfDayChart.width(620)
-                  .height(180)
-                  .margins({top: 10, right: 10, bottom: 30, left: 40})
-                  .dimension(hourOfDay)
-                  .group(hourOfDayGroup)
-                  .elasticY(true)
-                  .gap(1)
-                  .round(dc.round.floor)
-                  .x(d3.scale.linear().domain([0, 24]))
-                  .renderHorizontalGridLines(true)
-                  .filterPrinter(function (filters) {
-                      var filter = filters[0], s = "";
-                      s += parseInt(filter[0], 10) + "h -> " + parseInt(filter[1], 10) + "h";
-                      return s;
-                  })
-                  .xAxis()
-                  .tickFormat(function (v) {
-                      return v + "h";
-                  });
+  // commits by day
+  volumeChart.width(990)
+             .height(160)
+             .margins({top: 10, right: 10, bottom: 30, left: 40})
+             .dimension(commitByDays)
+             .group(commitByDaysGroup)
+             .elasticY(true)
+             .centerBar(true)
+             .gap(0)
+             .x(d3.time.scale().domain([commitByDays.bottom(1)[0].date, commitByDays.top(1)[0].date]))
+             .round(d3.time.day.round)
+             .xUnits(d3.time.days);
 
-    hourOfDayChart.yAxis().ticks(5);
+  // count selected commits
+  dc.dataCount(".dc-data-count").dimension(ndx)
+                                .group(all);
 
-    // day of the week
-    dayOfWeekChart.width(360)
-                  .height(30 * dayOfWeekGroup.size())
-                  .margins({top: 10, left: 10, right: 10, bottom: 30})
-                  .group(dayOfWeekGroup)
-                  .dimension(dayOfWeek)
-                  .colors(d3.scale.category10())
-                  .label(function (d) {
-                      return d.key.split(".")[1];
-                  })
-                  .title(function (d) {
-                      return d.value;
-                  })
-                  .elasticX(true)
-                  .xAxis().ticks(4);
-
-    // commits by day
-    volumeChart.width(990)
-               .height(160)
-               .margins({top: 10, right: 10, bottom: 30, left: 40})
-               .dimension(commitByDays)
-               .group(commitByDaysGroup)
-               .elasticY(true)
-               .centerBar(true)
-               .gap(0)
-               .x(d3.time.scale().domain([commitByDays.bottom(1)[0].date, commitByDays.top(1)[0].date]))
-               .round(d3.time.day.round)
-               .xUnits(d3.time.days);
-
-    // count selected commits
-    dc.dataCount(".dc-data-count").dimension(ndx)
-                                  .group(all);
-
-    // render everything
-    dc.renderAll();
-  }
-
-})(window);
+  // render everything
+  dc.renderAll();
+}
